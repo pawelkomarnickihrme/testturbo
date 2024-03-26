eb6a2b4912757c8fbc12b48f2becadf8bb48a7f5
pawelkomarnickihrme <124041781+pawelkomarnickihrme@users.noreply.github.com>
2024-03-26 13:21:03 +0100

    Add React import and update tsconfig.json

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Button from "@/app/Button/Button";
+import React from "react";
 
 export default function Page(): JSX.Element {
   return <Button onClick={() => console.log("click")}>ASD</Button>;
